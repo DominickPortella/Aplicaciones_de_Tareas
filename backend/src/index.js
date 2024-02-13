da2baf8ea9ce929934f87b0b65302d80b7f67240
DominickPortella <157386679+DominickPortella@users.noreply.github.com>
2024-02-12 23:04:56 -0500

    Delete backend directory

--- backend/src/index.js
+++
@@ -1,7 +0,0 @@
-import app from './app';
-
-const PORT = 3000; // Cambiar al puerto deseado, asegúrate de que esté disponible
-
-app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`);
-});
--- backend/src/swaggerOptions.js
+++
@@ -1,8 +0,0 @@
-export const options = {
-    definition: {
-        info:{
-            title: "tasks api"
-        }
-    },
-    apis: ["./src/routes/**/*.js"]
-}
